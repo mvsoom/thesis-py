Fix bug in MVN sample parametrized by precision matrix

vtr/prior/bandwidth.py
@@ -2,7 +2,6 @@
 from init import __memory__
 from vtr import spectrum
 from vtr.prior import formant
-from vtr.prior import allpole
 from lib import constants
 from dgf import bijectors
 from vtr.prior import pareto
@@ -148,7 +147,7 @@ def TFB_prior(cacheid=3325495):
 ##########################################
 SAMPLERARGS = {'sample': 'rslice', 'bootstrap': 10}
 RUNARGS = {'save_bounds': False, 'maxcall': int(1e7)}
-NUM_LOGL_SAMPLES = 1
+NUM_LOGL_SAMPLES = 1 #yolo
 
 @__memory__.cache
 def get_TFB_samples(
@@ -283,8 +282,8 @@ def yield_fitted_TFB_samples(
     vtfilter,
     seed,
     Ks,
-    fit_kwargs={},
-    parallel=(1,0)
+    parallel=(1,0),
+    **fit_kwargs
 ):
     TFB_samples = get_TFB_samples()
     rng = np.random.default_rng(seed)

vtr/prior/polezero.py
@@ -13,8 +13,8 @@ def _transform_by_precision_matrix(w, precision_matrix):
     Implementation: http://www.statsathome.com/2018/10/19/sampling-from-multivariate-normal-precision-and-covariance-parameterizations
     """
     try:
-        L = np.linalg.cholesky(precision_matrix)
-        z = scipy.linalg.solve_triangular(L, w, lower=True)
+        U = scipy.linalg.cholesky(precision_matrix) # Upper triangular
+        z = scipy.linalg.solve_triangular(U, w) # Backward substition
         return z
     except np.linalg.LinAlgError:
         return _nan_like(w)
@@ -141,10 +141,11 @@ class PoleZeroFilter:
         tilt = -10.*np.log10(4) # = -6 dB/octave
         return tilt
 
-def get_fitted_TFB_samples(n_jobs=1):
+def get_fitted_TFB_samples(n_jobs=1, **kwargs):
     return bandwidth.get_fitted_TFB_samples(
         n_jobs,
         vtfilter=PoleZeroFilter,
         seed=842329,
-        Ks=PoleZeroFilter.K_RANGE
+        Ks=PoleZeroFilter.K_RANGE,
+        **kwargs
     )